1f27636df7918ff2459343f89ba4a86d0304fc51
Justin Kay <justinkay92@gmail.com>
2025-06-14 19:09:58 -0500

    Remove unused accuracy function and torchmetrics

--- coda/oracle.py
+++ coda/oracle.py
@@ -2,10 +2,9 @@ import torch
 
 
 class Oracle:
-    def __init__(self, dataset, loss_fn=None, accuracy_fn=None):
+    def __init__(self, dataset, loss_fn=None):
         self.dataset = dataset
         self.loss_fn = loss_fn
-        self.accuracy_fn = accuracy_fn
         self.device = dataset.device
         self.labels = dataset.labels
         assert self.labels is not None, "Oracle needs labels!"
@@ -24,21 +23,6 @@ class Oracle:
         return self.loss_fn(preds.view(-1, C), self.labels.repeat(H), 
                             reduction='none').view(H, N).mean(dim=1)
 
-    def true_accuracies(self, preds):
-        H, N, C = preds.shape
-        accuracies = []
-        batch_size = 100  # Adjust this value based on your GPU memory
-        for i in range(0, H, batch_size):
-            batch_end = min(i + batch_size, H)
-            batch_preds = preds[i:batch_end]
-            batch_labels = self.labels
-            batch_acc = []
-            for j in range(batch_end - i):
-                acc = self.accuracy_fn(batch_preds[j], batch_labels)
-                batch_acc.append(acc)
-            accuracies.extend([acc.item() for acc in batch_acc])
-            torch.cuda.empty_cache()
-        return accuracies
 
     def __call__(self, idx):
         return self.labels[idx].item()
